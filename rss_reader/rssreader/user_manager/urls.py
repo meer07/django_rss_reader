578e39f3b70b385cbf7fb827e16a486f6ef20e76
海下 直哉 <naoya-kaige@garage.co.jp>
2014-12-17 15:19:43 +0900

    adding logout fixed layout

--- rss_reader/rssreader/user_manager/urls.py
+++ rss_reader/rssreader/user_manager/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
                        url(r'^register/$', views.register, name='register'),
                        url(r'^create_user/$', views.create_user, name='create_user'),
-                       url(r'^login/$', views.login, name='login'),)
+                       url(r'^login/$', views.login, name='login'),
+                       url(r'^logout/$', views.logout, name='logout'),)
--- rss_reader/rssreader/user_manager/views.py
+++ rss_reader/rssreader/user_manager/views.py
@@ -1,21 +1,28 @@
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.shortcuts import redirect
-from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse
 
 # Create your views here.
 
 def index(request):
-
-    return render_to_response('index.html', {}, context_instance=RequestContext(request))
+    dec = check_error_code(request)
+    return render_to_response('index.html', dec, context_instance=RequestContext(request))
 
 
 def register(request):
-    return render_to_response('register.html', {}, context_instance=RequestContext(request))
+    dec = check_error_code(request)
+    return render_to_response('register.html', dec, context_instance=RequestContext(request))
+
+def check_error_code(request):
+    if request.method == "GET" and request.GET.has_key('error'):
+        dec = {"error": str(request.GET['error'])}
+    else:
+        dec = {}
+    return dec
 
 def create_user(request):
     try:
@@ -23,18 +30,16 @@ def create_user(request):
         password = request.POST['password']
 
         if len(user_id) <= 0 or len(password) <= 0:
-
+            return redirect('/register?error=1')
 
         elif User.objects.filter(username=user_id):
-            return render_to_response('register.html', {"error":"2"}, context_instance=RequestContext(request))
+            return redirect('/index/register?error=2')
         else:
             new_user = User.objects.create_user(username=user_id, email=None, password=password)
             new_user.save()
             return redirect('/index')
     except:
-        return redirect('/register')
-
-
+        return redirect('/index/register?error=3')
 
 
 def login(request):
@@ -49,6 +54,12 @@ def login(request):
             user_page_url = '/user/?user_id=' + user_id
             return redirect(user_page_url)
         else:
-            return HttpResponseRedirect(index,args=[])
+            return redirect('/index?error=1')
     else:
-        return render_to_response('index.html', {"error": "2"}, context_instance=RequestContext(request))
+        return redirect('/index?error=2')
+
+
+def logout(request):
+    auth_logout(request)
+
+    return render_to_response('logout.html', {}, context_instance=RequestContext(request))
